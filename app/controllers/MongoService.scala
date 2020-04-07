da495361722e33f80707670d5dfba8b8f13c5ad6
Danny <dannypen@gmail.com>
2020-04-07 17:42:13 +0100

    Basic setup for MongoService & RegController

---
+++ app/controllers/MongoService.scala
@@ -0,0 +1,27 @@
+
+package controllers
+
+import javax.inject.Inject
+import models._
+import play.api.mvc._
+import reactivemongo.play.json.collection.{JSONCollection, _}
+
+import scala.concurrent.{Await, ExecutionContext, Future}
+import reactivemongo.play.json._
+import collection._
+import models.JsonFormats._
+import play.api.libs.json.{JsValue, Json}
+import reactivemongo.api.Cursor
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import reactivemongo.api.commands.WriteResult
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+
+class MongoService @Inject()(
+                              val reactiveMongoApi: ReactiveMongoApi
+                            ) extends ReactiveMongoComponents {
+
+  def userCollection: Future[JSONCollection] = reactiveMongoApi.database.map(_.collection[JSONCollection]("users"))
+
+
+}
