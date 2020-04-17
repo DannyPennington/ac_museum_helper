7ce39295084e970a706e9d47a95f136b41ef60c2
Danny <dannypen@gmail.com>
2020-04-17 16:43:53 +0100

    Added user database searching functions

--- app/controllers/MongoService.scala
+++ app/controllers/MongoService.scala
@@ -6,7 +6,7 @@ import models._
 import play.api.mvc._
 import reactivemongo.play.json.collection.{JSONCollection, _}
 
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future}
 import reactivemongo.play.json._
 import collection._
 import models.JsonFormats._
@@ -15,7 +15,7 @@ import reactivemongo.api.Cursor
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 import reactivemongo.api.commands.WriteResult
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
+
 
 class MongoService @Inject()(
                               val reactiveMongoApi: ReactiveMongoApi
@@ -23,7 +23,31 @@ class MongoService @Inject()(
 
   def userCollection: Future[JSONCollection] = reactiveMongoApi.database.map(_.collection[JSONCollection]("users"))
 
-  def addUser(user:User): Future[WriteResult] = {
+  def addUser(user: User): Future[WriteResult] = {
     userCollection.flatMap(_.insert.one(user))
+  }
+
+  def findUserEmail(email: String): Future[Option[User]] = {
+    userCollection.map {
+      _.find(Json.obj("email" -> email))
+        .sort(Json.obj("email" -> -1))
+        .cursor[User]()
+    }.flatMap(
+      _.collect[List](
+        1,
+        Cursor.FailOnError[List[User]]()
+      )
+    )
+      .map(_.headOption)
+  }
+
+  def userEmailExists(email: String): Future[Boolean] = {
+    findUserEmail(email).map {
+      _.getOrElse(None)
+      match {
+        case None => false
+        case _ => true
+      }
+    }
   }
 }
